6705f40e51c0ae8a61a265ee0e27a2f405ec07a3
anton.karunarathne <anton.karunarathne@gmail.com>
2024-01-19 15:13:03 +1030

    BEG-137: code refinement changes

--- Model/Api/PrerenderRecachingManagement.php
+++ Model/Api/PrerenderRecachingManagement.php
@@ -8,24 +8,11 @@ declare(strict_types=1);
 
 namespace Aligent\Prerender\Model\Api;
 
-use Magento\Framework\Exception\BulkException;
-use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Serialize\Serializer\Json;
-use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
-use Magento\Sales\Api\OrderRepositoryInterface;
-use Magento\Sales\Api\Data\OrderInterface;
 use Aligent\Prerender\Api\PrerenderRecachingManagementInterface;
 use Aligent\Prerender\Api\Data\PrerenderRecachingManagementRequestInterface;
 use Aligent\Prerender\Api\PrerenderClientInterface;
-use Magento\Sales\Model\Order;
 use Exception;
-use DOMDocument;
-use DOMException;
-use Magento\Store\Model\App\Emulation;
-use Magento\Framework\Escaper;
-use Magento\Framework\Stdlib\DateTime\DateTime;
-use Magento\Directory\Model\CountryFactory;
-use Magento\Framework\Exception\CouldNotSaveException;
 
 class PrerenderRecachingManagement implements PrerenderRecachingManagementInterface
 {
