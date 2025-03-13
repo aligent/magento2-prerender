a6016a9b47b0c71d009b0988aca898ffef6c3154
ryan.jehan <ryan.jehan@aligent.com.au>
2025-03-13 11:03:19 +1030

    Fixed the property name to

--- Model/Indexer/Category/ProductIndexer.php
+++ Model/Indexer/Category/ProductIndexer.php
@@ -36,7 +36,7 @@ class ProductIndexer implements IndexerActionInterface, MviewActionInterface, Di
     /** @var PrerenderClientInterface  */
     private PrerenderClientInterface $prerenderClient;
     /** @var DeploymentConfig  */
-    private DeploymentConfig $eploymentConfig;
+    private DeploymentConfig $deploymentConfig;
     /** @var Config  */
     private Config $prerenderConfigHelper;
     /** @var Configurable */
